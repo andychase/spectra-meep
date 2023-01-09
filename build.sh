eb12a877a612a523b8267af404ffb7cf45a686a9
Andy Chase <theandychase@gmail.com>
2023-01-09 22:53:42 +0000

    New build instructions

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
-#!bash
-docker buildx build \
-  --platform arm64 \
-  -t 414380061457.dkr.ecr.us-east-1.amazonaws.com/spectra_meep:latest \
-  --push \
-  .
+apt-get update &&  \
+    apt-get install -y \
+    build-essential \
+    swapspace \
+    csh gfortran libatlas-base-dev libxc-dev curl cmake
+[ $(arch) == "aarch64" ] && mv install.arm64.info install.info
+csh ./tools/lapack/download-lapack.csh 
+make -j4 lapack
+sed -i 's/uname -p/uname -m/g' ./ddi/compddi
+csh ../ddi/compddi
+cp ./ddi/ddikick.x .
+make libxc -j4
+make modules
+make
+
