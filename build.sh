517358bc47fc6fde85e1c4d78e826a0b4e1c85da
Andy Chase <theandychase@gmail.com>
2023-01-12 15:34:45 -0800

    More scripts!

--- build.sh
+++ build.sh
@@ -5,16 +5,18 @@ apt-get update &&  \
     build-essential \
     swapspace \
     csh gfortran libatlas-base-dev libxc-dev curl cmake
-[ "$(arch)" == "aarch64" ] && mv install.arm64.info install.info
-make clean
-find . -name "*.o" -type f -delete
-csh ./tools/lapack/download-lapack.csh
-make -j4 lapack
-sed -i 's/uname -p/uname -m/g' ./ddi/compddi
-csh ./ddi/compddi
-cp ./ddi/ddikick.x .
-make libxc -j4
-[ "$(arch)" == "aarch64" ] && grep -rl -- "-mcmodel=medium" . | xargs sed -i "s/-mcmodel=medium/-mcmodel=small/g"
-make modules
-./compall
-make
+if [ "$(arch)" == "aarch64" ]; then
+  mv install.arm64.info install.info
+  make clean
+  find . -name "*.o" -type f -delete
+  csh ./tools/lapack/download-lapack.csh
+  make -j4 lapack
+  sed -i 's/uname -p/uname -m/g' ./ddi/compddi
+  csh ./ddi/compddi
+  cp ./ddi/ddikick.x .
+  make libxc -j4
+  grep -rl -- "-mcmodel=medium" . | xargs sed -i "s/-mcmodel=medium/-mcmodel=small/g"
+  make modules
+  ./compall
+  make
+fi
---
+++ run_image.sh
@@ -0,0 +1,31 @@
+set -e
+set -x
+# oracle
+if grep -Fxq 'NAME="Oracle Linux Server"' /etc/os-release
+then
+dnf update -y
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf install docker-ce -y
+systemctl start docker
+systemctl enable docker
+
+else
+
+wget https://get.docker.com -O o.sh
+bash o.sh
+wget https://bootstrap.pypa.io/get-pip.py -O get-pip.py
+python3 get-pip.py
+
+fi
+
+python3 -m pip install pip --upgrade
+python3 -m pip install awscli
+mkdir ~/.aws || true
+cat << EOF > ~/.aws/credentials
+[default]
+aws_access_key_id = AKIAWA6XZ6MIX5AAXYU5
+aws_secret_access_key = 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
+EOF
+python3 -m awscli ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 414380061457.dkr.ecr.us-east-1.amazonaws.com
+docker pull 414380061457.dkr.ecr.us-east-1.amazonaws.com/spectra_meep
+docker run 414380061457.dkr.ecr.us-east-1.amazonaws.com/spectra_meep bash -c "while true; do python ./src/aws.py; sleep 10; done"
