df8f1819078678694c55f849dd427862b95173fb
Andy Chase <theandychase@gmail.com>
2023-01-20 16:05:52 -0800

    Pass through hostname to containers

--- script/build_containers.sh
+++ script/build_containers.sh
@@ -1,4 +1,4 @@
-#docker buildx build --platform=linux/arm64 -t 414380061457.dkr.ecr.us-east-1.amazonaws.com/spectra_meep:arm --push .
+docker buildx build --platform=linux/arm64 -t 414380061457.dkr.ecr.us-east-1.amazonaws.com/spectra_meep:arm --push .
 docker buildx build --platform=linux/amd64 -t 414380061457.dkr.ecr.us-east-1.amazonaws.com/spectra_meep:amd --push .
 docker manifest rm 414380061457.dkr.ecr.us-east-1.amazonaws.com/spectra_meep:latest
 docker manifest create \
